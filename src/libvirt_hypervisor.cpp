8ef5de62758c48dc59ba5cc32713ecbde1022475
Tim Ohliger <tim.ohliger@rwth-aachen.de>
2014-12-22 17:12:17 +0100

    Added implementation to start, stop and migrate methods of Libvirt_hypervisor.

--- src/libvirt_hypervisor.cpp
+++ src/libvirt_hypervisor.cpp
@@ -4,24 +4,85 @@
 
 #include <libvirt/libvirt.h>
 #include <stdexcept>
+#include <thread>
+#include <memory>
+
+
+/// TODO: Get hostname dynamically.
+Libvirt_hypervisor::Libvirt_hypervisor() :
+	local_host_conn(virConnectOpen("qemu+ssh://localhost/system"))
+{
+	if (!local_host_conn)
+		throw std::runtime_error("Failed to connect to qemu on local host.");
+}
+
+Libvirt_hypervisor::~Libvirt_hypervisor()
+{
+	std::unique_lock<std::mutex> lock(thread_counter_mutex);
+	while (thread_counter != 0)
+		thread_counter_cv.wait(lock);
+	if (!virConnectClose(local_host_conn))
+		LOG_PRINT(LOG_WARNING, "Some qemu connections have not been closed after destruction of hypervisor wrapper!");
+}
 
 void Libvirt_hypervisor::start(const std::string &vm_name, size_t vcpus, size_t memory)
 {
-	LOG_STREAM(LOG_DEBUG, "vm_name: " << vm_name << ", vcpus: " << vcpus << ", memory " << memory);
-	LOG_PRINT(LOG_ERR, "No implementation of start in libvirt hypervisor wrapper!");
-	throw std::logic_error("start not implemented.");
+	virDomainPtr d1 = virDomainLookupByName(local_host_conn, vm_name.c_str());
+	std::unique_ptr<virDomain, decltype(&virDomainFree)> domain(d1, virDomainFree);
+	if (!domain)
+		throw std::runtime_error("Domain not found.");
+	std::unique_ptr<virDomainInfo> domain_info(new virDomainInfo);
+	if (virDomainGetInfo(domain.get(), domain_info.get()) == -1)
+		throw std::runtime_error("Failed getting domain info.");
+	if (domain_info->state != VIR_DOMAIN_SHUTOFF)
+		throw std::runtime_error("Wrong domain state: " + std::to_string(domain_info->state));
+	if (virDomainCreate(domain.get()) == -1)
+		throw std::runtime_error("Error creating domain.");
+	if (virDomainSetVcpus(domain.get(), vcpus) == -1)
+		throw std::runtime_error("Setting number of vcpus to " + std::to_string(vcpus) + " for domain " + vm_name);
+	if (virDomainSetMemory(domain.get(), memory) == -1)
+		throw std::runtime_error("Setting memory to " + std::to_string(vcpus) + " KiB for domain " + vm_name);
 }
 
 void Libvirt_hypervisor::stop(const std::string &vm_name)
 {
-	LOG_STREAM(LOG_DEBUG, "vm_name: " << vm_name);
-	LOG_PRINT(LOG_ERR, "No implementation of stop in libvirt hypervisor wrapper!");
-	throw std::logic_error("stop not implemented.");
+	virDomainPtr d1 = virDomainLookupByName(local_host_conn, vm_name.c_str());
+	std::unique_ptr<virDomain, decltype(&virDomainFree)> domain(d1, virDomainFree);
+	//std::unique_ptr<virDomain> domain(virDomainLookupByName(local_host_conn, vm_name.c_str()), virDomainFree);
+	if (!domain)
+		throw std::runtime_error("Domain not found.");
+	std::unique_ptr<virDomainInfo> domain_info(new virDomainInfo);
+	if (virDomainGetInfo(domain.get(), domain_info.get()) == -1)
+		throw std::runtime_error("Failed getting domain info.");
+	if (domain_info->state != VIR_DOMAIN_RUNNING)
+		throw std::runtime_error("Domain not running.");
+	if (virDomainDestroy(domain.get()) == -1)
+		throw std::runtime_error("Error destroying domain.");
 }
 
 void Libvirt_hypervisor::migrate(const std::string &vm_name, const std::string &dest_hostname, bool live_migration)
 {
-	LOG_STREAM(LOG_DEBUG, "vm_name: " << vm_name << ", dest_hostname: " << dest_hostname << ", live_migration " << live_migration);
-	LOG_PRINT(LOG_ERR, "No implementation of migrate in libvirt hypervisor wrapper!");
-	throw std::logic_error("migrate not implemented.");
+	virDomainPtr d1 = virDomainLookupByName(local_host_conn, vm_name.c_str());
+	std::unique_ptr<virDomain, decltype(&virDomainFree)> domain(d1, virDomainFree);
+	//std::unique_ptr<virDomain> domain(virDomainLookupByName(local_host_conn, vm_name.c_str()), virDomainFree);
+	if (!domain)
+		throw std::runtime_error("Domain not found.");
+	std::unique_ptr<virDomainInfo> domain_info(new virDomainInfo);
+	if (virDomainGetInfo(domain.get(), domain_info.get()) == -1)
+		throw std::runtime_error("Failed getting domain info.");
+	if (domain_info->state != VIR_DOMAIN_RUNNING)
+		throw std::runtime_error("Domain not running.");
+	virConnectPtr c1 = virConnectOpen(("qemu+ssh://" + dest_hostname + "/system").c_str());
+	std::unique_ptr<virConnect, decltype(&virConnectClose)> dest_connection(c1, virConnectClose);
+	//std::unique_ptr<virConnect> dest_connection(
+	//		virConnectOpen(("qemu+ssh://" + dest_hostname + "/system").c_str()),
+	//		virConnectClose);
+	if (!dest_connection)
+		throw std::runtime_error("Cannot establish connection to " + dest_hostname);
+	unsigned long flags = 0;
+	flags |= live_migration ? VIR_MIGRATE_LIVE : VIR_MIGRATE_OFFLINE;
+	virDomainPtr d2 = virDomainLookupByName(dest_connection.get(), vm_name.c_str());
+	std::unique_ptr<virDomain, decltype(&virDomainFree)> dest_domain(d2, virDomainFree);
+	if (!dest_domain)
+		throw std::runtime_error("Cannot establish connection to " + dest_hostname);
 }
--- src/libvirt_hypervisor.hpp
+++ src/libvirt_hypervisor.hpp
@@ -3,13 +3,29 @@
 
 #include "hypervisor.hpp"
 
+#include <libvirt/libvirt.h>
+#include <mutex>
+#include <condition_variable>
+
 class Libvirt_hypervisor :
 	public Hypervisor
 {
 public:
+	/// TODO: Consider copy/move constructor implementation.
+	Libvirt_hypervisor();
+	~Libvirt_hypervisor();
 	void start(const std::string &vm_name, size_t vcpus, size_t memory);
 	void stop(const std::string &vm_name);
 	void migrate(const std::string &vm_name, const std::string &dest_hostname, bool live_migration);
+private:
+	void start_task();
+	void stop_task();
+	void migrate_task();
+
+	virConnectPtr local_host_conn;	
+	unsigned int thread_counter;
+	std::mutex thread_counter_mutex;
+	std::condition_variable thread_counter_cv;
 };
 
 #endif
