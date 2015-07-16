d98289c103aa2e41152b9be70538c9e0729739ff
Tim Ohliger <tim.ohliger@rwth-aachen.de>
2015-07-16 12:35:08 +0200

    Cleaned Libvirt_hypervisor by adding deleter for virConnect and virDomain and removed unnecessary std::unique_ptr for virDomainInfo.

--- src/libvirt_hypervisor.cpp
+++ src/libvirt_hypervisor.cpp
@@ -18,6 +18,21 @@
 #include <fstream>
 #include <sstream>
 
+struct Deleter_virConnect
+{
+	void operator()(virConnectPtr ptr) const
+	{
+		virConnectClose(ptr);
+	}
+};
+
+struct Deleter_virDomain
+{
+	void operator()(virDomainPtr ptr) const
+	{
+		virDomainFree(ptr);
+	}
+};
 
 /// TODO: Get hostname dynamically.
 Libvirt_hypervisor::Libvirt_hypervisor() :
@@ -37,16 +52,17 @@ Libvirt_hypervisor::~Libvirt_hypervisor()
 void Libvirt_hypervisor::start(const std::string &vm_name, unsigned int vcpus, unsigned long memory)
 {
 	// Get domain by name
-	virDomainPtr d1 = virDomainLookupByName(local_host_conn, vm_name.c_str());
-	std::unique_ptr<virDomain, decltype(&virDomainFree)> domain(d1, virDomainFree);
+	std::unique_ptr<virDomain, Deleter_virDomain> domain(
+		virDomainLookupByName(local_host_conn, vm_name.c_str())
+	);
 	if (!domain)
 		throw std::runtime_error("Domain not found.");
 	// Get domain info + check if in shutdown state
-	std::unique_ptr<virDomainInfo> domain_info(new virDomainInfo);
-	if (virDomainGetInfo(domain.get(), domain_info.get()) == -1)
+	virDomainInfo domain_info;
+	if (virDomainGetInfo(domain.get(), &domain_info) == -1)
 		throw std::runtime_error("Failed getting domain info.");
-	if (domain_info->state != VIR_DOMAIN_SHUTOFF)
-		throw std::runtime_error("Wrong domain state: " + std::to_string(domain_info->state));
+	if (domain_info.state != VIR_DOMAIN_SHUTOFF)
+		throw std::runtime_error("Wrong domain state: " + std::to_string(domain_info.state));
 	// Set memory
 	if (virDomainSetMemoryFlags(domain.get(), memory, VIR_DOMAIN_AFFECT_CONFIG | VIR_DOMAIN_MEM_MAXIMUM) == -1)
 		throw std::runtime_error("Error setting maximum amount of memory to " + std::to_string(memory) + " KiB for domain " + vm_name);
@@ -65,15 +81,16 @@ void Libvirt_hypervisor::start(const std::string &vm_name, unsigned int vcpus, u
 void Libvirt_hypervisor::stop(const std::string &vm_name)
 {
 	// Get domain by name
-	virDomainPtr d1 = virDomainLookupByName(local_host_conn, vm_name.c_str());
-	std::unique_ptr<virDomain, decltype(&virDomainFree)> domain(d1, virDomainFree);
+	std::unique_ptr<virDomain, Deleter_virDomain> domain(
+		virDomainLookupByName(local_host_conn, vm_name.c_str())
+	);
 	if (!domain)
 		throw std::runtime_error("Domain not found.");
 	// Get domain info + check if in running state
-	std::unique_ptr<virDomainInfo> domain_info(new virDomainInfo);
-	if (virDomainGetInfo(domain.get(), domain_info.get()) == -1)
+	virDomainInfo domain_info;
+	if (virDomainGetInfo(domain.get(), &domain_info) == -1)
 		throw std::runtime_error("Failed getting domain info.");
-	if (domain_info->state != VIR_DOMAIN_RUNNING)
+	if (domain_info.state != VIR_DOMAIN_RUNNING)
 		throw std::runtime_error("Domain not running.");
 	// Destroy domain
 	if (virDomainDestroy(domain.get()) == -1)
@@ -113,29 +130,32 @@ void detach_device(virDomainPtr domain)
 void Libvirt_hypervisor::migrate(const std::string &vm_name, const std::string &dest_hostname, bool live_migration)
 {
 	// Get domain by name
-	virDomainPtr d1 = virDomainLookupByName(local_host_conn, vm_name.c_str());
-	std::unique_ptr<virDomain, decltype(&virDomainFree)> domain(d1, virDomainFree);
+	std::unique_ptr<virDomain, Deleter_virDomain> domain(
+		virDomainLookupByName(local_host_conn, vm_name.c_str())
+	);
 	if (!domain)
 		throw std::runtime_error("Domain not found.");
 	// Get domain info + check if in running state
-	std::unique_ptr<virDomainInfo> domain_info(new virDomainInfo);
-	if (virDomainGetInfo(domain.get(), domain_info.get()) == -1)
+	virDomainInfo domain_info;
+	if (virDomainGetInfo(domain.get(), &domain_info) == -1)
 		throw std::runtime_error("Failed getting domain info.");
-	if (domain_info->state != VIR_DOMAIN_RUNNING)
+	if (domain_info.state != VIR_DOMAIN_RUNNING)
 		throw std::runtime_error("Domain not running.");
 	// Detach devices TODO: RAII handler and dynamic device recognition.
 	detach_device(domain.get());
 	// Connect to destination
-	virConnectPtr c1 = virConnectOpen(("qemu+ssh://" + dest_hostname + "/system").c_str());
-	std::unique_ptr<virConnect, decltype(&virConnectClose)> dest_connection(c1, virConnectClose);
+	std::unique_ptr<virConnect, Deleter_virConnect> dest_connection(
+		virConnectOpen(("qemu+ssh://" + dest_hostname + "/system").c_str())
+	);
 	if (!dest_connection)
 		throw std::runtime_error("Cannot establish connection to " + dest_hostname);
 	// Set migration flags
 	unsigned long flags = 0;
 	flags |= live_migration ? VIR_MIGRATE_LIVE : 0;
 	// Migrate domain
-	virDomainPtr d2 = virDomainMigrate(domain.get(), dest_connection.get(), flags, 0, 0, 0);
-	std::unique_ptr<virDomain, decltype(&virDomainFree)> dest_domain(d2, virDomainFree);
+	std::unique_ptr<virDomain, Deleter_virDomain> dest_domain(
+		virDomainMigrate(domain.get(), dest_connection.get(), flags, 0, 0, 0)
+	);
 	if (!dest_domain)
 		throw std::runtime_error(std::string("Migration failed: ") + virGetLastErrorMessage());
 	// Attach device
